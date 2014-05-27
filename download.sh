317cb3fbcbdcdfd5871587b2cda2df5e6a030d13
kyndek <kyndek@gmail.com>
2014-05-28 00:40:29 +0400

    Updated download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 langlist="ar ast be bg bn_BD br ca cs da de el en_GB en_US es_AR es_ES et eu fi fr fy ga gd gl he hr hu hy id is it ja ko lt nb_NO nl nn_NO pa_IN pl pt_BR pt_PT ro ru si sk sl sq sr sv_SE ta_LK tr uk vi zh_CN zh_TW"
 
-tversion=24.4.0
+tversion=24.5.0
 
 for i in $langlist;do wget http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/$tversion/linux-i686/xpi/$i.xpi;done
