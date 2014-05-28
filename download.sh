23cd7484f875ecc3fdc333a13cceb0e8ea79185c
kyndek <kyndek@gmail.com>
2014-05-28 11:34:40 +0400

    Updated download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-langlist="ar ast be bg bn_BD br ca cs da de el en_GB en_US es_AR es_ES et eu fi fr fy ga gd gl he hr hu hy id is it ja ko lt nb_NO nl nn_NO pa_IN pl pt_BR pt_PT ro ru si sk sl sq sr sv_SE ta_LK tr uk vi zh_CN zh_TW"
+langlist="ar ast be bg bn-BD br ca cs da de el en-GB en-US es-AR es-ES et eu fi fr fy-NL ga-IE gd gl he hr hu hy-AM id is it ja ko lt nb-NO nl nn-NO pa-IN pl pt-BR pt-PT ro ru si sk sl sq sr sv-SE ta-LK tr uk vi zh-CN zh-TW"
 
 tversion=24.5.0
 
