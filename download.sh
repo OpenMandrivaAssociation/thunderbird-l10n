2f105f43dea51ee6ca106914f0924d117df50259
crisb <crisb@localhost.localdomain>
2015-04-02 11:51:42 +0100

    Obsolete, merged back into main thunderbird spec

--- download.sh
+++
@@ -1,5 +0,0 @@
-langlist="ar ast be bg bn-BD br ca cs da de el en-GB en-US es-AR es-ES et eu fi fr fy-NL ga-IE gd gl he hr hu hy-AM id is it ja ko lt nb-NO nl nn-NO pa-IN pl pt-BR pt-PT ro ru si sk sl sq sr sv-SE ta-LK tr uk vi zh-CN zh-TW"
-
-tversion=`grep ^Version: thunderbird-l10n.spec  | awk '{print $2}'`
-
-for i in $langlist;do wget http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/$tversion/linux-i686/xpi/$i.xpi;done
